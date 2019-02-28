c37827fd8b213158240d549f6118b009792d39d5
John Pfuntner <jpfuntne@cisco.com>
2019-02-28 14:19:17 -0500

    Teaching br about notepad
    br now uses:
      vi (vim) by default
      gvim when -g/--gvim is specified (Windoze only)
      notepad when -n/--notepad is specified (Windoze only)

---
+++ bin/truepath.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import logging
+import argparse
+
+def process(rawpath, log):
+  path = os.path.abspath(rawpath)
+  if 'win' in sys.platform:
+    log.debug('Sorry to see you\'re using Windoze...')
+    path = 'C:\\cygwin64' + path.replace('/', '\\')   
+  else:
+    log.debug('You are not using Windoze like a boss!')
+  log.debug('{rawpath!r} => {path!r}'.format(**locals()))
+  return path
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Get `true` path - useful on Windoze')
+  parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Enable debugging')
+  parser.add_argument('paths', metavar='path', nargs='+', help='One or more paths')
+  args = parser.parse_args()
+  
+  logging.basicConfig(format='%(asctime)s %(levelname)s %(pathname)s:%(lineno)d %(msg)s')
+  log = logging.getLogger()
+  log.setLevel(logging.DEBUG if args.verbose else logging.WARNING)
+  
+  for path in args.paths:
+    print process(path, log)
