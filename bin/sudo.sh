70089c919e9d99617cd978d05ced3ac65d378987
John Pfuntner <jpfuntne@cisco.com>
2019-09-12 08:04:17 -0400

    Stylistic change to sudo.sh

--- bin/sudo.sh
+++ bin/sudo.sh
@@ -1,3 +1,3 @@
 # This script will be used from aliases to get to run commands like minikube and kubectl from root.  I want an informational message reminding me this is happening.
-echo \> sudo "$@" >&2 \<
+echo \> sudo "$@" \< >&2
 sudo "$@"
