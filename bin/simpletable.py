7bd9a556684fc7b353e27c68f00d74273c81c3af
John Pfuntner <jpfuntne@cisco.com>
2020-05-13 09:20:50 -0400

    Creating a couple of new tools
    simpletable.py is a simple Python2 version of table.py that just uses the basic Unix `column` utility to form the output.

---
+++ bin/simpletable.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+import logging
+import argparse
+import subprocess
+
+class Table(object):
+  def __init__(self, *args):
+    if len(args) == 1 and type(args[0]) in (list, tuple):
+      self.headings = args[0]
+    else:
+      self.headings = args
+
+    self.p = subprocess.Popen(['column', '-t', '-s\t'], stdin=subprocess.PIPE)
+    self.p.stdin.write('\t'.join([str(heading) for heading in self.headings]) + '\n')
+
+  def add(self, *args):
+    self.p.stdin.write('\t'.join([str(arg) for arg in args]) + '\n')
+
+  def close(self):
+    self.p.stdin.close()
+    self.p.wait()
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Test simpletable')
+  parser.add_argument('-v', '--verbose', action='count', help='Enable debugging')
+  args = parser.parse_args()
+  
+  logging.basicConfig(format='%(asctime)s %(levelname)s %(pathname)s:%(lineno)d %(msg)s')
+  log = logging.getLogger()
+  log.setLevel(logging.WARNING - (args.verbose or 0)*10)
+
+  table = Table('col1', 'col2')
+  table.add('r1c1', 'r1c2')
+  table.add('row 2, column 1', 'row 2, column 2')
+  table.close()
