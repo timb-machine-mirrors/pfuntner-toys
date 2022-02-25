007843565a08182eadc79068f8b355a297b13f96
John Pfuntner <jpfuntne@cisco.com>
2022-02-25 12:49:08 -0500

    Creating new tool

---
+++ bin/supercd.sh
@@ -0,0 +1,20 @@
+if [ "$BASH_SOURCE" == "$0" ]
+then
+  echo "Note: $0 is not sourced" >&2
+fi
+
+eval files=$(supercd.py --bash "$@")
+
+if [ ${#files[@]} -eq 1 ]
+then
+  cd "${files[0]}"
+elif [ ${#files[@]} -eq 0 ]
+then
+  echo "No matches for $@" >&2
+else
+  echo "Too many matches for $@" >&2
+  for file in "${files[@]}"
+  do
+    echo "  $file"
+  done
+fi
