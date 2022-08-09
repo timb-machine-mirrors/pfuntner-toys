268a474fd4eb7a0e25dfd5c7a4b6d5d71e0cea9f
U-CISCO\jpfuntne <CISCO+jpfuntne@DESKTOP-2RSKU8V.cisco.com>
2022-08-09 14:43:41 -0400

    Adding --glob option to treat pattern as a bash-style pattern

--- bin/supercd.py
+++ bin/supercd.py
@@ -3,15 +3,22 @@
 import re
 import os
 import signal
+import fnmatch
 import logging
 import argparse
+
+def match(filename):
+  if args.glob:
+    return fnmatch.fnmatch(filename, args.pat)
+  else:
+    return regexp.search(filename)
 
 def dive(path):
   ret = list()
 
   if not ('/.' in path or '/Downloads' in path or '/third-party' in path or '/__pycache__' in path):
 
-    if regexp.search(os.path.basename(path)) and os.path.isdir(path) and not os.path.islink(path):
+    if match(os.path.basename(path)) and os.path.isdir(path) and not os.path.islink(path):
       ret.append(path)
   
     if os.path.isdir(path):
@@ -35,6 +42,7 @@ def protect_metachars(s):
 
 parser = argparse.ArgumentParser(description='`Super cd` Python code')
 parser.add_argument('pat', help='Pattern to search for')
+parser.add_argument('-g', '--glob', action='store_true', help='Use bash-style glob pattern matching')
 parser.add_argument('-b', '--bash', action='store_true', help='Generate output to set bash array')
 parser.add_argument('-v', '--verbose', action='count', help='Enable debugging')
 args = parser.parse_args()
@@ -45,7 +53,8 @@ log.setLevel(logging.WARNING - (args.verbose or 0)*10)
 
 signal.signal(signal.SIGPIPE, lambda signum, stack_frame: exit(0))
 
-regexp = re.compile(args.pat)
+regexp = None if args.glob else re.compile(args.pat)
+
 files = dive(os.path.expanduser("~"))
 
 if args.bash:
