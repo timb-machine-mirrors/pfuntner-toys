a4f61f6454bfd6beb401e1657fd9bb291072f303
John Pfuntner <pfuntner@pobox.com>
2022-11-19 08:23:16 -0500

    Improving the comment
    I created the link and tried using it but it worked slightly differently than I expected.  I adjusted the example usage command and added a comment about doing follwoing the redirected URL.

--- misc/clone.sh
+++ misc/clone.sh
@@ -1,5 +1,7 @@
 # This command will clone Bruno's toys repo.  I will create a friendly link the dump file like so:
 # 
-#   curl http://tinyurl.com/pfuntner-install-toys | bash
+#   curl -L https://tinyurl.com/pfuntner-install-toys | bash
+#
+# The -L option is used to follow the `redirect` from tinyurl.
 #
 git clone https://github.com/pfuntner/toys.git
