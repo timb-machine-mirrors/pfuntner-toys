2983c431a69558f2ab0cfd54d69c164ff1a8af5f
John Pfuntner <pfuntner@pobox.com>
2022-11-19 08:18:28 -0500

    Creating command to clone my repo easily

---
+++ misc/clone.sh
@@ -0,0 +1,5 @@
+# This command will clone Bruno's toys repo.  I will create a friendly link the dump file like so:
+# 
+#   curl http://tinyurl.com/pfuntner-install-toys | bash
+#
+git clone https://github.com/pfuntner/toys.git
