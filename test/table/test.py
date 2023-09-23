66d2dda28a3c58fe5dd55ecc5b5b1b7031a543ae
John Pfuntner <pfuntner@pobox.com>
2023-09-23 09:50:54 -0400

    Python 3 migration
    I haven't used this script in a while and it was still trying using Python 2.  I don't even have Python 2 on the system I was using - only Python3.
    
    I also made use of format strings - which truly rock!
    
    There's still a bug I'm trying to track down.

--- test/table/test.py
+++ test/table/test.py
@@ -1,30 +1,17 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 
 import os
 import re
 import sys
 import logging
 import argparse
-import subprocess
 
-def run(cmd, stdin=None, forgive=False):
-  if isinstance(cmd, str):
-    cmd = cmd.split()
-  p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  (stdout, stderr) = p.communicate(stdin or '')
-  rc = p.wait()
-  fail = (rc != 0) or stderr
-  log.log(logging.ERROR if fail else logging.DEBUG,
-          '{cmd}: {rc}, {stdout!r}, {stderr!r}'.format(**locals()))
-  if fail:
-    exit(1)
-  return (rc, stdout, stderr)
+import bruno_tools
 
 def mkdir(*path):
   dir = os.path.join(*path)
   exists = os.path.isdir(dir)
-  log.log(logging.DEBUG if exists or args.create else logging.ERROR,
-          '{dir} exists: {state}'.format(state=exists, **locals()))
+  log.log(logging.DEBUG if exists or args.create else logging.ERROR, f'{dir} exists: {exists}')
   if not exists:
     if args.create:
       os.mkdir(dir)
@@ -38,41 +25,40 @@ def process(input, output, actual_string, *path):
     else:
       expected_string = stream.read()
       if actual_string != expected_string:
-        log.critical('{input} -> {output} mismatch'.format(**locals()))
+        log.debug(f'{expected_string=!r} {actual_string=!r}')
+        log.critical(f'{input} -> {output} mismatch')
         exit(1)
 
 parser = argparse.ArgumentParser(description='Test table script')
 parser.add_argument('-c', '--create', dest='create', action='store_true', help='Create output')
-parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Enable debugging')
+parser.add_argument('-v', '--verbose', dest='verbose', action='count', help='Enable debugging')
 args = parser.parse_args()
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(pathname)s:%(lineno)d %(msg)s')
 log = logging.getLogger()
-log.setLevel(logging.DEBUG if args.verbose else logging.WARNING)
+log.setLevel(logging.WARNING - (args.verbose or 0)*10)
 
 test_dir = os.path.dirname(sys.argv[0])
 table_script = os.path.join(test_dir, '..', '..', 'bin', 'table.py')
-log.debug('{table_script}: {exists}'.format(exists=os.path.exists(table_script), **locals()))
+log.debug(f'{table_script}: {os.path.exists(table_script)}')
 
 #   -i {csv,yaml,fixed,json,separator}, --input {csv,yaml,fixed,json,separator}
-(rc, stdout, stderr) = run([table_script, '--help'], forgive=True)
+(rc, stdout, stderr) = bruno_tools.run([table_script, '--help'], log=log)
 # log.debug(repr(stdout))
 hits = re.findall(r'--(\S+)\s+\{([^}]+)\}', stdout, flags=re.MULTILINE)
 methods = {hit[0]: hit[1].split(',') for hit in hits}
-log.debug('methods: {methods}'.format(**locals()))
+log.debug(f'methods: {methods}')
 
 for curr_test in [dir for dir in os.listdir(test_dir) if os.path.isdir(os.path.join(test_dir, dir))]:
-  print 'Testing {curr_test}'.format(**locals())
+  print(f'Testing {curr_test}')
   with open(os.path.join(test_dir, curr_test, 'original.txt')) as original_stream:
     original = original_stream.read()
     for headings in [[], ['--headings']]:
       for input in methods['input']:
-        (rc, input_string, stderr) = run([table_script, '-i', 'fixed', '-o', input] + headings, stdin=original)
+        (rc, input_string, stderr) = bruno_tools.run([table_script, '-i', 'fixed', '-o', input] + headings, stdin=original, log=log)
         mkdir(test_dir,curr_test, input)
         for output in methods['output']:
           mkdir(test_dir, curr_test, input, output)
 
-          (rc, output_string, stderr) = run([table_script, '--regexp', r'\|',  '-i', input, '-o', output] + headings,
-                                            stdin=input_string)
-          process(input, output, output_string, test_dir, curr_test, input, output,
-                  '{state}headings.expected'.format(state='' if headings else 'no'))
+          (rc, output_string, stderr) = bruno_tools.run([table_script, '--regexp', r'\|',  '-i', input, '-o', output] + headings, stdin=input_string, log=log)
+          process(input, output, output_string, test_dir, curr_test, input, output, f'{"" if headings else "no"}headings.expected')
