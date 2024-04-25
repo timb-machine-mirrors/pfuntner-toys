93a5d15341dc5d600eeafcd489326bd1a9ba78b3
John Pfuntner <jpfuntne@cisco.com>
2024-04-25 13:11:30 -0400

    Fixing minor bug
    I had made a change a while ago that appended ~/.ssh/config.keep to ~/.ssh/config but injected a bug: the contents of the extra file were added for each and every cloud instance added to ~/.ssh/config.  I'm changing the logic to append **after** all of the cloud instances have been added.

--- bin/instances.py
+++ bin/instances.py
@@ -641,9 +641,10 @@ if __name__ == '__main__':
         with open(ssh_config_filename, 'w') as stream:
           for instance in instances:
             stream.write(f'Host {instance.name}\n\tHostname {instance.ip}\n\tUser {instance.user}\n\tIdentityFile {instance.key_filename}\n\tProxyCommand nc -X connect -x proxy.esl.cisco.com:80 %h %p\n')
-            if os.path.exists(ssh_config_keep_filename):
-              with open(ssh_config_keep_filename) as keep_config:
-                stream.write(keep_config.read())
+
+          if os.path.exists(ssh_config_keep_filename):
+            with open(ssh_config_keep_filename) as keep_config:
+              stream.write(keep_config.read())
 
         active_instances = [instance.name for instance in instances if instance.active]
         if active_instances and not args.no_fingerprints:
