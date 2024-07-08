4464af02f75c098da17623a34ec656c426168ce7
John Pfuntner <jpfuntne@cisco.com>
2024-07-08 14:14:47 -0400

    Switching over to using find shell command
    Originally the script would search for the pattern mostly using Python code.  After I switched my work machine to WSL, this proved to be too slow but using the shell find command works better.  The command only uses glob-style patterns as opposed to regular expressions but I think that's a fine compromise.
    
    At first I was thinking I would make use of the -follow predicate of the find command but I didn't think it was necessary.  I can add it if I want.

--- bin/supercd.py
+++ bin/supercd.py
@@ -7,43 +7,10 @@ import fnmatch
 import logging
 import argparse
 
-def match(filename):
-  if args.glob:
-    return fnmatch.fnmatch(filename, args.pat)
-  else:
-    return regexp.search(filename)
-
-def dive(path):
-  ret = list()
-
-  if not ('/.' in path or '/Downloads' in path or '/third-party' in path or '/__pycache__' in path):
-
-    if os.path.isdir(path):
-      log.debug(f'Trying {path!r}')
-      if match(os.path.basename(path)):
-        ret.append(path)
-
-      children = list()
-      try:
-        children = os.listdir(path)
-      except Exception as e:
-        log.info(f'listdir({path!r}) error: {e!r}')
-      for filename in children:
-        ret += dive(os.path.join(path, filename))
-
-  return ret
-
-def protect_metachars(s):
-  ret = ''
-  for c in s:
-    if c in '"$':
-      ret += '\\'
-    ret += c
-  return f'"{ret}"'
+import bruno_tools
 
 parser = argparse.ArgumentParser(description='`Super cd` Python code')
-parser.add_argument('pat', help='Pattern to search for')
-parser.add_argument('-g', '--glob', action='store_true', help='Use bash-style glob pattern matching')
+parser.add_argument('pat', help='Glob pattern to search for')
 parser.add_argument('-b', '--bash', action='store_true', help='Generate output to set bash array')
 parser.add_argument('-v', '--verbose', action='count', help='Enable debugging')
 args = parser.parse_args()
@@ -54,9 +21,7 @@ log.setLevel(logging.WARNING - (args.verbose or 0)*10)
 
 signal.signal(signal.SIGPIPE, lambda signum, stack_frame: exit(0))
 
-regexp = None if args.glob else re.compile(args.pat)
-
-files = dive(os.path.expanduser("~"))
+files = bruno_tools.run(['find', os.path.expanduser('~'), '-name', args.pat], log=log)[1].splitlines()
 
 if args.bash:
   print(f'({" ".join(files)})')
